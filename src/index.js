c8a7deffc447e72cb706b6f7770291654f31fa56
Alexey-Khlebnikov <Alexey.Khlebnikov@outlook.com>
2020-05-23 02:43:46 +1000

    init starter project * material ui * redux * redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
+import { Router, Switch, Route } from "react-router-dom";
+
+import { ThemeProvider } from "@material-ui/core/styles";
+
+import * as serviceWorker from "./serviceWorker";
+import "./index.css";
+
+import routingHistory from "./app/RoutingHistory";
+
+import App from "./app/App";
+import store from "./store";
+import theme from "./theme/default";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={ store }>
+			<ThemeProvider theme={ theme }>
+				<Router history={ routingHistory }>
+					<Switch>
+						<Route path="/" component={ App } />
+					</Switch>
+				</Router>
+			</ThemeProvider>
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById("root"),
 );
 
 // If you want your app to work offline and load faster, you can change
